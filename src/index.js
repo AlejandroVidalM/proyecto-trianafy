76db403d433633ecb9b45a288069a0d872c45e7e
AlejandroVidalM <alejandro.vidal.marin.18@gmail.com>
2021-01-26 20:27:27 +0100

    Modelos de cancion, playlist y usuarios

--- src/index.js
+++ src/index.js
@@ -8,3 +8,20 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import morganBody from 'morgan-body';
 
+import mongoose from "mongoose"
+
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+  
+  if (err) {
+    console.log(`Error de conexión a la base de datos: ${JSON.stringify(err)}`);
+  } else {
+    console.log(`Conexión correcta a la base de datos en la URI ${process.env.DB_URI}`);
+    app.listen(process.env.PORT, () =>
+      console.log(
+        `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
+      )
+    );
+  }
+
+});
