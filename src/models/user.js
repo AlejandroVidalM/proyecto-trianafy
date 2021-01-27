a199ec5d83f56152f880dd38b6e7c7b581fec537
Alejandro Vidal Marín <vidal.maale20@aulas.triana>
2021-01-27 14:24:40 +0100

    JWT a empezar

--- src/models/user.js
+++ src/models/user.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
+    fullname: String,
     username: String,
     email: String,
     password: String
-
 });
+
 const User = mongoose.model('User', userSchema);
 
 export {userSchema, User}
